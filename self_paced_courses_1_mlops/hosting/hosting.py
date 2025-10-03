16ed9d2388fa0b587bc5cbcf627c053a7ca48d41
umangagarwal-1008 <umang.agarwal@greatlearning.in>
2025-10-03 20:50:31 +0530

    Update hosting.py

--- self_paced_courses_1_mlops/hosting/hosting.py
+++ self_paced_courses_1_mlops/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="self_paced_courses_1_mlops/deployment",     # the local folder containing your files
-    repo_id="PASTE_YOUR_REPO_ID",          # the target repo
+    repo_id="umangagarwal1008/PIMA-Diabetes-Prediction",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
